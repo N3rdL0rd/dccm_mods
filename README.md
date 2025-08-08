31561e1fc0595f89eeb5cc7c0ff77336ea84e5de
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-08-08 18:24:44 -0500

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# N3rdL0rd's Lazy DCCM Mods
+
+This repo is a collection of mods I've written with DCCM that exist for very specific things.
+
+## Okay sure I'll bite. What is there?
+
+- [TripleGold](./TripleGold/) - Triples all gold you pick up - [download]()
+
---
+++ TripleGold/TripleGold.cs
@@ -0,0 +1,20 @@
+﻿using dc.en.hero;
+using HaxeProxy.Runtime;
+using ModCore.Mods;
+
+namespace TripleGold
+{
+    public class TripleGoldMod(ModInfo info) : ModBase(info)
+    {
+        private void Hook_beheaded_addMoney(Hook_Beheaded.orig_addMoney orig, Beheaded self, int val, Ref<bool> noStats)
+        {
+            val *= 3;
+            orig(self, val, noStats);
+        }
+
+        public override void Initialize()
+        {
+            Hook_Beheaded.addMoney += Hook_beheaded_addMoney;
+        }
+    }
+}
